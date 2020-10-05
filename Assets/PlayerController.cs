5c6e977e90b9753007ddc5901c0685484abdf485
Devansh-299 <f20180131@goa.bits-pilani.ac.in>
2020-10-05 08:02:34 +0530

    navmesh - basic implementation

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+    
+    public Camera mCamera;
+    public NavMeshAgent player;
+
+    // Update is called once per frame
+    void Update() {
+    	/*
+			0 -> for Left Mouse button
+			1 -> for Right Mouse button 
+    	*/
+        if (Input.GetMouseButtonDown(0)) {
+        	Debug.Log("Mouse Clicked");
+        	// to make player react to mouse clicks
+        	Ray ray = mCamera.ScreenPointToRay(Input.mousePosition);
+        	RaycastHit hit;
+
+        	if(Physics.Raycast(ray, out hit)) {
+        		// code to move the agent
+        		Debug.Log("Valid");
+        		player.SetDestination(hit.point);
+        	}
+        }
+    }
+}
