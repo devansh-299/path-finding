ed547ef466436a5cadd3d82501db2a368882e704
Devansh-299 <f20180131@goa.bits-pilani.ac.in>
2020-11-18 23:05:16 +0530

    navmesh implementation in a multi-floor building

---
+++ Assets/NavAgentController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class NavAgentController : MonoBehaviour {
+    
+    public Camera mCamera;
+    public NavMeshAgent player;
+
+    // Update is called once per frame
+    void Update() {
+    	/*
+			0 -> for Left Mouse button
+			1 -> for Right Mouse button 
+    	*/
+        if (Input.GetMouseButtonDown(0)) {
+        	// to make player react to mouse clicks
+        	Ray ray = mCamera.ScreenPointToRay(Input.mousePosition);
+        	RaycastHit hit;
+
+        	if(Physics.Raycast(ray, out hit) && !EventSystem.current.IsPointerOverGameObject()) {
+        		// code to move the agent
+        		Debug.Log("Valid");
+        		player.SetDestination(hit.point);
+        	}
+        }
+    }
+}
---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    public FixedJoystick cameraStick;
+
+    protected float cameraAngle;
+    protected float cameraAngleSpeed = 2f;
+    
+	void Update () {
+		float horizontalMovement = cameraStick.input.x * cameraAngleSpeed;
+		float verticalMovement = cameraStick.input.y * cameraAngleSpeed;
+		Camera.main.transform.Rotate (-verticalMovement, horizontalMovement, 0);
+
+		Vector3 desiredPosition = target.position + offset;
+    	Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+    	transform.position = smoothedPosition;
+	}
+}
